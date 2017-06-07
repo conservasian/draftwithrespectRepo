1cf21c3994306694d62893b441a3ad495bb7a923
Patrick <patrick.p.ye@gmail.com>
2017-06-06 22:06:56 -0700

    csv to json for incidents

---
+++ incidents2json.py
@@ -0,0 +1,37 @@
+# incidents2json.py
+# Patrick Ye
+
+import csv
+
+playerName = []
+assaultRelated = []
+status = []
+details = []
+link = []
+
+with open('#dwr - Incidents.csv', 'rb') as csvfile:
+	filereader = csv.reader(csvfile)
+	for row in filereader:
+		playerName.append(row[0])
+		assaultRelated.append(row[4])
+		status.append(row[6])
+		details.append(row[5])
+		link.append(row[11])
+
+
+import json
+
+data = {}
+data["incidents"] = []
+
+for k in range(len(playerName)):
+    data["incidents"].append({
+    	"Name":playerName[k],
+    	"AssaultRelated":assaultRelated[k],
+		"Status":status[k],
+		"Details":details[k],
+		"Link":link[k],
+    })
+
+with open('incidents.json', 'w') as f:
+     json.dump(data, f)
